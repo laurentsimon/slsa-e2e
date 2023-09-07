9b6355c5d3e52edd7aa707a162f0d7c24df5a916
laurentsimon <laurentsimon@google.com>
2023-09-07 23:22:16 +0000

    update
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- .github/workflows/scripts/verify-policy.sh
+++ .github/workflows/scripts/verify-policy.sh
@@ -12,9 +12,7 @@ validate_path "${UNTRUSTED_USER_POLICY}"
 trusted_path="${UNTRUSTED_USER_POLICY}"
 
 ./policy-verifier eval \
-    --files "__THIS_REPO__/.slsa/policy.json,${trusted_path}" \
+    --files ".slsa/policy.json,${trusted_path}" \
     --source-uri "${UNTRUSTED_REPOSITORY}" \
     --image-uri "${UNTRUSTED_MUTABLE_IMAGE}" \
     --builder-id "${UNTRUSTED_BUILDER_ID}"
-
-#go run . eval --labels 'key1=val1, key2=val2' --files 'pkg/policy/testdata/org.json,pkg/policy/testdata/repo.json' --source-uri git+https://github.com/googlenot/repo1  
